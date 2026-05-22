b60e5ad21032f05702ed74bb345f3f4579e2379d
Guoyang Luo <104761532+Hasakev@users.noreply.github.com>
2026-05-22 15:49:24 +1000

    fix: copy_object date range metadata copying (#143)

--- tests/test_plexosdb_copy_object.py
+++ tests/test_plexosdb_copy_object.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -38,6 +39,52 @@ def test_copy_object(db_base: PlexosDB, caplog):
             assert new_properties[old_property] != original_object_id
         else:
             assert old_properties[old_property] == new_properties[old_property]
+
+
+def test_copy_object_copies_date_from_and_date_to(db_base: PlexosDB):
+    from plexosdb import ClassEnum
+
+    db = db_base
+    original_object_name = "TestGenDates"
+    object_class = ClassEnum.Generator
+    db.add_object(object_class, original_object_name)
+
+    date_from = datetime(2026, 1, 1)
+    date_to = datetime(2026, 12, 31)
+    original_data_id = db.add_property(
+        ClassEnum.Generator,
+        original_object_name,
+        "Max Capacity",
+        100.0,
+        date_from=date_from,
+        date_to=date_to,
+    )
+
+    original_date_from = db.query(
+        "SELECT date FROM t_date_from WHERE data_id = ?", (original_data_id,)
+    )
+    original_date_to = db.query(
+        "SELECT date FROM t_date_to WHERE data_id = ?", (original_data_id,)
+    )
+    assert original_date_from == [(date_from.isoformat(),)]
+    assert original_date_to == [(date_to.isoformat(),)]
+
+    new_object_name = "TestGenDatesCopy"
+    db.copy_object(object_class, original_object_name, new_object_name)
+
+    new_data_ids = db.get_object_data_ids(object_class, name=new_object_name)
+    assert len(new_data_ids) == 1
+    new_data_id = new_data_ids[0]
+    assert new_data_id != original_data_id
+
+    copied_date_from = db.query(
+        "SELECT date FROM t_date_from WHERE data_id = ?", (new_data_id,)
+    )
+    copied_date_to = db.query(
+        "SELECT date FROM t_date_to WHERE data_id = ?", (new_data_id,)
+    )
+    assert copied_date_from == [(date_from.isoformat(),)]
+    assert copied_date_to == [(date_to.isoformat(),)]
 
 
 def test_copy_object_with_memberships(db_base: PlexosDB):
